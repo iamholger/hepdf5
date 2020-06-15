e3a62298652d123e4c1d25822874bb1e74f257da
Holger Schulz <hschulz@fnal.gov>
2020-06-15 10:39:51 -0500

    loop

--- testHandle.cc
+++ testHandle.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "HepMC3/WriterAscii.h"
 #include "Handle.h"
 
 using namespace hepdf5;
@@ -7,13 +8,30 @@ using namespace hepdf5;
 int main(int argc, char ** argv)
 {
     auto handle = Handle(std::string(argv[1]));
-    std::cerr << "num events: " << handle.nevents() << "\n";
+    //std::cerr << "num events: " << handle.nevents() << "\n";
 
-    //for (auto s : handle.start_p(0,100)) std::cerr << s << "\n";
-    //for (auto s : handle.getZ(0,100, "start_p")) std::cerr << s << "\n";
-    //for (auto s : handle.get<int>(0, 100, std::string("start_p"))) std::cerr << s << "\n";
-    //for (auto s : handle.get<double>(0, 100, std::string("vertex/x"))) std::cerr << s << "\n";
+    size_t numread=0;
+    size_t nread=atoi(argv[2]);
 
-    readEvents(handle, 0, 100);
-  return 0;
+  std::shared_ptr<GenRunInfo> run = std::make_shared<GenRunInfo>();;
+    //WriterAscii writer("blabla.hepmc3", run);
+  ////for (auto g : genevents) Print::listing(g);
+  //for (auto g : genevents) writer.write_event(g);
+    GenEvent _evt(Units::GEV,Units::MM);//, run);
+    while (numread<handle.nevents())
+    {
+      if (handle.nevents() - numread < nread) nread = handle.nevents() - numread;
+      handle.fillBuffers(numread, nread);
+
+      for (size_t i=0; i<nread;++i)
+      {
+        handle.fillEvent(i, _evt);
+        //writer.write_event(_evt);
+      }
+      //if (numread==0) 
+        //Print::listing(_evt);
+      numread+=nread;
+    }
+
+    return 0;
 }
